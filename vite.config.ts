5e6a7b4a111a8ea140436c4751683602378c9574
Ishita Patil <118566917+ishitapatil08@users.noreply.github.com>
2026-06-23 19:47:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  // Force enable the nitro deploy plugin for Vercel
+  nitro: {
+    preset: "vercel"
+  },
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
     server: { 
-      entry: "server",
-      preset: "vercel" // <--- Add this line!
+      entry: "server"
     },
   },
 });
